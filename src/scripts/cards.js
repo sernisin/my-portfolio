843562b7fae70bdb7db6ee1fad40ffcef4e0647f
Your Name <you@example.com>
2020-12-07 12:58:16 +0400

    first

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,11 +1,8 @@
 import $ from "jquery";
 // images
-import p3 from "../images/p3.jpg";
-import p2 from "../images/p2.jpg";
-import p1 from "../images/p1.jpg";
-import light from "../images/lights.jpg";
-import mountain from "../images/mountains.jpg";
-import nature from "../images/nature.jpg";
+import p3 from "../images/pexelsphoto3985166.jpg";
+import p2 from "../images/medical-appointment-doctor-healthcare-40568.jpg";
+import p1 from "../images/paper.jpg";
 
 let card = [
   {
@@ -25,27 +22,6 @@ let card = [
   {
     id: "photos",
     cardImg: p1,
-    cardTitle: "Lorem Ipsum",
-    cardText:
-      "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla."
-  },
-  {
-    id: "design",
-    cardImg: light,
-    cardTitle: "Lorem Ipsum",
-    cardText:
-      "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla."
-  },
-  {
-    id: "art",
-    cardImg: mountain,
-    cardTitle: "Lorem Ipsum",
-    cardText:
-      "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla."
-  },
-  {
-    id: "photos",
-    cardImg: nature,
     cardTitle: "Lorem Ipsum",
     cardText:
       "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla."
--- src/scripts/prices.js
+++ src/scripts/prices.js
@@ -1,59 +1,38 @@
 import $ from "jquery";
 let price = [
   {
-    base: "Basic",
-    oneH3: "Web Design",
-    twoH3: "Photography",
-    threeH3: "5GB Storage",
-    fourH3: "Mail Support",
-    fiveH3: "$10",
-    fiveH3Span: "per month",
-    button: "Sign Up"
+    base: "COVID-19",
+    oneH3: "Müayinə üçün tələblər:",
+    twoH3: "Sizin üçün təyin olunmuş saatda COVID-19 testi üçün klinikaya gələn zaman acqarına olmağınız və ya son qida, maye qəbulundan 2-3 saat keçməsi mütləqdir.  ",
+    threeH3: "Onlayn qeydiyyatdan keçən zaman öncədən Sizin üçün təyin olunmuş saatda müvafiq mərkəzimizə yaxınlaşacaqsınız.",
+    fiveH3: "$10"
   },
   {
-    base: "Pro",
-    oneH3: "Web Design",
-    twoH3: "Photography",
-    threeH3: "50GB Storage",
-    fourH3: "Endless Support",
-    fiveH3: "$25",
-    fiveH3Span: "per month",
-    button: "Sign Up"
+    base: "General",
+    oneH3: "Əməliyyat öncəsi müayinəƏməliyyat öncəsi müayinə",
+    twoH3: "Diabetik skrininq",
+    threeH3: "Onlayn qeydiyyatdan keçən zaman öncədən Sizin üçün təyin olunmuş saatda müvafiq mərkəzimizə yaxınlaşacaqsınız.",
+    fiveH3: "$25"
   },
   {
-    base: "Premium",
-    oneH3: "Web Design",
-    twoH3: "Photography",
-    threeH3: "Unlimited Storage",
-    fourH3: "Endless Support",
-    fiveH3: "$25",
-    fiveH3Span: "per month",
-    button: "Sign Up"
+    base: "Special",
+    oneH3: "Dietoloji paket",
+    twoH3: "Saç tökülməsi",
+    threeH3: "Onlayn qeydiyyatdan keçən zaman öncədən Sizin üçün təyin olunmuş saatda müvafiq mərkəzimizə yaxınlaşacaqsınız.",
+    fiveH3: "$25"
   }
 ];
 $(document).ready(function() {
   price.map(function(el) {
     $(".price").append(
       `<div class="price-card">
-              <div>
-                <h3 class="base">${el.base}</h3>
+              <div><h3 class="base">${el.base}</h3></div>
+              <div> <h3>${el.oneH3}</h3></div>
+              <div><h3>${el.twoH3}</h3></div>
+              <div><h3>${el.threeH3}</h3></div>
+              <div> <h3 class="bases">${el.fiveH3}</h3>
               </div>
-              <div>
-                <h3>${el.oneH3}</h3>
-              </div>
-              <div>
-                <h3>${el.twoH3}</h3>
-              </div>
-              <div>
-                <h3>${el.threeH3}</h3>
-              </div>
-              <div>
-                <h3>${el.fourH3}</h3>
-              </div>
-              <div>
-                <h3 class="bases">${el.fiveH3} <br /><span>${el.fiveH3Span}</span></h3>
-              </div>
-              <div><button>${el.button}</button></div>
+              <div><button>TAKE</button></div>
             </div>`
     );
   });
